09effc40894158e407f348d89489015266bd0c05
Michal <viniarski@gmail.com>
2024-01-24 00:16:05 +0000

    add event to prev and next buttons fetch API get 10 images

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 // fetch images from unsplash with API
 
 const form = document.getElementById("form");
-const imagecontainer = document.getElementById("img_container");
+const imageContainer = document.getElementById("img_container");
+
+let gallery = [];
 
 form.addEventListener("submit", function (event) {
   event.preventDefault();
-  console.log(event);
   let query = event.target.input.value;
   search(query);
 });
@@ -14,22 +15,37 @@ async function search(queryParam) {
   let response = await fetch(
     `https://api.unsplash.com/search/photos?page=1&query=${queryParam}&client_id=ywoppbhOsnIKpTA0ekfrY_sjIkmImzjMj8TAj2o4jig`
   );
-  console.log(response);
   let data = await response.json();
-  
+
   if (data.results.length > 0) {
-    imagecontainer.innerHTML = '';
-
-    data.results.forEach((result) => {
-      let img = document.createElement("img");
-      img.src = result.urls.raw;
-      imagecontainer.appendChild(img);
-    });
+    gallery = data.results.slice(0, 10); // fetch 10 pictures
+    showImage(currentIndex);
+  }
 }
 
+function showImage(index) {
+  imageContainer.innerHTML = `<img src="${gallery[index].urls.raw}" alt="Image">`;
 }
 
-// left and right nav buttons
+// next and previous button, works in circle
 
-// const buttonNext = 
-// const buttonPrevious = 
+const buttonNext = document.getElementById("next");
+const buttonPrevious = document.getElementById("previous");
+
+let currentIndex = 0;
+
+buttonNext.addEventListener("click", function () {
+  currentIndex++;
+  if (currentIndex >= gallery.length) {
+    currentIndex -= gallery.length;
+  }
+  showImage(currentIndex);
+});
+
+buttonPrevious.addEventListener("click", function () {
+  currentIndex--;
+  if (currentIndex < 0) {
+    currentIndex += gallery.length;
+  }
+  showImage(currentIndex);
+});
